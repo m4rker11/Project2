68a3aec9182ae0d75cda72ba8ff686a9b3db4314
m4rker11 <m4rker11@gmail.com>
2022-04-09 11:43:43 -0400

    communication between js and python one way

--- webpage/app.js
+++ webpage/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const axios = require('axios');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set('view engine', 'hbs');
@@ -12,7 +12,33 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     console.log(req.body);
-    res.render('index');
+    res.render('tweets');
+});
+app.post('/addTwitter', (req, res) => {
+    console.log(req.body);
+    axios.post("http://127.0.0.1:5000/receiver", 
+    {
+    // method: 'POST',
+    // headers: {
+    // 'Content-type': 'application/json',
+    // 'Accept': 'application/json'
+    // },
+    // Strigify the payload into JSON:
+    body:JSON.stringify(req.body)}).then(res=>{
+    if(res.ok){
+    return res.json()
+    }else{
+    alert("something is wrong")
+    }
+    }).then(jsonResponse=>{
+    
+    // Log the response data in the console
+    console.log(jsonResponse)
+    } 
+    ).catch((err) => console.error(err));
+    
+    
+    res.render('tweets');
 });
 
 app.listen(3000, () => {
